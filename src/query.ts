91793e344ad4fa9da219bd9a8755042c4a55dbb8
ducktrshessami <jwee761@gmail.com>
2023-06-30 16:18:49 -0700

    query parsing

--- src/query.ts
+++ src/query.ts
@@ -1,5 +1,8 @@
 import { RollQueryError } from "./error";
 
+export const RollQueryPattern = /^(?:[+-]?\s*(?:\d*d)?\d+)(?:\s*[+-]\s*(?:\d*d)?\d+)*$/i;
+export const RollQueryItemPattern = /(?<sign>[+-])?\s*(?:(?<count>\d*)d)?(?<sides>\d+)/gi;
+
 function validateAttribute(n: number, label: string): void {
     if (n < 1 || Math.floor(n) !== n) {
         const value = typeof n === "string" ? `'${n}'` : n;
@@ -18,12 +21,41 @@ export class RollQueryItem {
     }
 }
 
+export type RollQueryOptions = {
+    items?: RollQueryItem[];
+    constant?: number;
+};
+
 export class RollQuery {
     items: RollQueryItem[];
     constant: number;
 
-    constructor() {
-        this.items = [];
-        this.constant = 0;
+    constructor({ items, constant }: RollQueryOptions = {}) {
+        this.items = items ?? [];
+        this.constant = constant ?? 0;
+    }
+
+    static parse(query: string): RollQuery | null {
+        if (!RollQueryPattern.test(query)) {
+            return null;
+        }
+        const q = new RollQuery();
+        const matches = query.matchAll(RollQueryItemPattern);
+        for (const match of matches) {
+            if (match.groups?.count == null) {
+                q.constant += parseInt(match.groups!.sides) * (match.groups!.sign === "-" ? -1 : 1);
+            }
+            else try {
+                q.items.push(new RollQueryItem(
+                    match.groups.count ? parseInt(match.groups.count) : 1,
+                    parseInt(match.groups.sides),
+                    match.groups.sign === "-"
+                ));
+            }
+            catch {
+                return null;
+            }
+        }
+        return q;
     }
 }
