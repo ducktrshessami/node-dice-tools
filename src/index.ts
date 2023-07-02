d57f38c3d3c29f7e41e2087d9847751d7b9210ab
ducktrshessami <jwee761@gmail.com>
2023-07-02 11:32:14 -0700

    feat: roll method set/get

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ export {
     MultiRollResult,
     RollMethod,
     RollResult,
+    getRollMethod,
     roll,
     rollAdvantage,
     rollDisadvantage,
-    rollMulti
+    rollMulti,
+    setRollMethod
 } from "./roll";
 export { RollQueryItemPattern, RollQueryPattern } from "./validate";
--- src/roll.ts
+++ src/roll.ts
@@ -1,12 +1,20 @@
 import { RollResultParseError } from "./error";
 import { validateDiceAttributes, validateNonEmptyArray } from "./validate";
 
+export type RollMethod = (sides: number) => number;
+let rollMethod: RollMethod | null = null;
+
+export function setRollMethod(method: RollMethod): void {
+    rollMethod = method;
+}
+
 function defaultRollMethod(sides: number): number {
     return Math.ceil(Math.random() * sides);
 }
 
-export type RollMethod = (sides: number) => number;
-let rollMethod: RollMethod = defaultRollMethod;
+export function getRollMethod(): RollMethod {
+    return rollMethod ?? defaultRollMethod;
+}
 
 export class RollResult {
     public readonly raw: readonly number[];
@@ -76,9 +84,10 @@ export class MultiRollResult {
 }
 
 export function rawRoll(count: number, sides: number): RollResult {
+    const method = getRollMethod();
     const result: number[] = [];
     for (let i = 0; i < count; i++) {
-        result.push(rollMethod(sides));
+        result.push(method(sides));
     }
     return new RollResult(result);
 }
